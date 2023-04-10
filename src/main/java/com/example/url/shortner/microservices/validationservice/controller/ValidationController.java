8acf484d54aea1991b0b1854dee3e6c92fcc048e
suraj <suraj200981@yahoo.co.uk>
2023-04-10 19:24:47 +0100

    working on validation services

--- src/main/java/com/example/url/shortner/microservices/validationservice/controller/ValidationController.java
+++ src/main/java/com/example/url/shortner/microservices/validationservice/controller/ValidationController.java
@@ -1,15 +1,24 @@
 package com.example.url.shortner.microservices.validationservice.controller;
 
 import com.example.url.shortner.microservices.validationservice.model.Url;
+import com.example.url.shortner.microservices.validationservice.validation.UrlValidation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URL;
+
 @RestController
 public class ValidationController {
-
+    @Autowired
+    UrlValidation urlValidation;
     @PostMapping("/validation")
     public void validationCheck(@RequestBody Url url ){
         System.out.println("Url posted: "+url.getUrl());
+
+        if(urlValidation.validateURL(url.getUrl())){
+            //proceed with forwarding request to shorten-service
+        }
     }
 }
--- src/main/java/com/example/url/shortner/microservices/validationservice/validation/UrlValidation.java
+++ src/main/java/com/example/url/shortner/microservices/validationservice/validation/UrlValidation.java
@@ -1,8 +1,16 @@
 package com.example.url.shortner.microservices.validationservice.validation;
 
+import com.example.url.shortner.microservices.validationservice.model.Url;
+
 public class UrlValidation {
 
-    public UrlValidation(){
-
+    private String url;
+    public UrlValidation(String url){
+        this.url=url;
     }
+
+    public Boolean validateURL(String url){
+        return true;
+    }
+
 }
