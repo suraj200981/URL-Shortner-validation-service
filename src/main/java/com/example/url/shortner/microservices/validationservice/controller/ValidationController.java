c923f83d5c1fdfd90b2e01a5b95afcc7cee96b64
suraj <suraj200981@yahoo.co.uk>
2023-04-10 18:36:42 +0100

    simple post request for testing connection to server done

--- src/main/java/com/example/url/shortner/microservices/validationservice/controller/ValidationController.java
+++ src/main/java/com/example/url/shortner/microservices/validationservice/controller/ValidationController.java
@@ -1,4 +1,15 @@
 package com.example.url.shortner.microservices.validationservice.controller;
 
+import com.example.url.shortner.microservices.validationservice.model.Url;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ValidationController {
+
+    @PostMapping("/validation")
+    public void validationCheck(@RequestBody Url url ){
+        System.out.println("Url posted: "+url.getUrl());
+    }
 }
---
+++ src/main/java/com/example/url/shortner/microservices/validationservice/model/Url.java
@@ -0,0 +1,21 @@
+package com.example.url.shortner.microservices.validationservice.model;
+
+public class Url {
+
+    private String url;
+
+    public Url(){
+
+    }
+    public Url(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
