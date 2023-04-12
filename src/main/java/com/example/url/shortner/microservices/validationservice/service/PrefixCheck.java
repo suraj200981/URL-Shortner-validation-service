3fbc4d2bcdad1adc95085e29f81b332d655298bc
suraj <suraj200981@yahoo.co.uk>
2023-04-12 19:32:17 +0100

    working on url shortener

---
+++ src/main/java/com/example/url/shortner/microservices/validationservice/service/PrefixCheck.java
@@ -0,0 +1,32 @@
+package com.example.url.shortner.microservices.validationservice.service;
+
+public class PrefixCheck {
+
+
+        public Boolean checkPrefix(String url){
+            if(prefixStringInspector(url).equals("false")) {
+                return false;
+            } else if (prefixStringInspector(url).equals("http://")) {
+                System.out.println("Correct prefix found");
+                return true;
+            } else if (prefixStringInspector(url).equals("https://")) {
+                System.out.println("Correct prefix found");
+                return true;
+            }
+        return false;
+        }
+
+
+        public String prefixStringInspector(String prefix) {
+            if (prefix.startsWith("http://")) {
+                prefix = prefix.substring(0, 7);
+                return prefix;
+            } else if (prefix.startsWith("https://")) {
+                prefix = prefix.substring(0, 8);
+                return prefix;
+            } else {
+                return "false";
+            }
+        }
+
+}
--- src/main/java/com/example/url/shortner/microservices/validationservice/validation/UrlValidation.java
+++ src/main/java/com/example/url/shortner/microservices/validationservice/validation/UrlValidation.java
@@ -2,6 +2,7 @@ package com.example.url.shortner.microservices.validationservice.validation;
 
 import com.example.url.shortner.microservices.validationservice.model.Url;
 import com.example.url.shortner.microservices.validationservice.service.LengthCheck;
+import com.example.url.shortner.microservices.validationservice.service.PrefixCheck;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,10 +12,17 @@ public class UrlValidation {
     @Autowired
     LengthCheck lengthCheck;
 
+    @Autowired
+    PrefixCheck prefixCheck;
+
     public Boolean validateURL(String url){
         if(lengthCheck.checkLength(url)){
 
             System.out.println("proceed with prefix check");
+
+            if (prefixCheck.checkPrefix(url)) {
+
+            }
 
         }else {
             throw new RuntimeException("Failed url length check/dot check failed");
