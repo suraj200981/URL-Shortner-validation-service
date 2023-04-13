36330492d9c3091b0d8fdf463e253c0028646be9
suraj <suraj200981@yahoo.co.uk>
2023-04-13 17:44:20 +0100

    deleted url shortener class as the new url generation will happen in the shorten-service

--- src/main/java/com/example/url/shortner/microservices/validationservice/validation/UrlValidation.java
+++ src/main/java/com/example/url/shortner/microservices/validationservice/validation/UrlValidation.java
@@ -1,14 +1,14 @@
 package com.example.url.shortner.microservices.validationservice.validation;
 
-import com.example.url.shortner.microservices.validationservice.model.Url;
-import com.example.url.shortner.microservices.validationservice.service.GenerateShortUrl;
 import com.example.url.shortner.microservices.validationservice.service.LengthCheck;
 import com.example.url.shortner.microservices.validationservice.service.PrefixCheck;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 
 @Component
+@Slf4j
 public class UrlValidation {
     @Autowired
     LengthCheck lengthCheck;
@@ -16,24 +16,24 @@ public class UrlValidation {
     @Autowired
     PrefixCheck prefixCheck;
 
-    @Autowired
-    GenerateShortUrl generateShortUrl;
 
     public Boolean validateURL(String url){
         if(lengthCheck.checkLength(url)){
 
-            System.out.println("proceed with prefix check");
+            log.info("Proceed with prefix check");
 
             if (prefixCheck.checkPrefix(url)) {
 
-                System.out.println(generateShortUrl.generateNewUrl(url, ""));
+                log.info("Validation checks of url have passed");
 
             }else {
-                System.out.println(generateShortUrl.generateNewUrl(url, "http://"));
+//                System.out.println(generateShortUrl.generateNewUrl(url, "http://"));
             }
             return true;
 
         }else {
+            log.error("Validation checks of url have failed");
+
             throw new RuntimeException("Failed url length check/dot check failed");
         }
     }
